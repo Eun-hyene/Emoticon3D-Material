f84d245fb2dd38d9d4dbb620dcf11e53cdaf3bd2
Eunhye <dkssud4875@naver.com>
2019-12-06 00:51:13 +0900

    상단바전체제거

--- src/components/BottomNav/index.jsx
+++ src/components/BottomNav/index.jsx
@@ -1,64 +1,64 @@
-import React, { Component } from 'react';
-import Button from 'react-md/lib/Buttons';
-import { Link } from 'gatsby';
-import config from '../../../data/SiteConfig';
-import './BottomNav.scss';
+import React, { Component } from "react";
+import Button from "react-md/lib/Buttons";
+import { Link } from "gatsby";
+import config from "../../../data/SiteConfig";
+import "./BottomNav.scss";
 
 class BottomNav extends Component {
   render() {
     return (
-      <footer className='footer footer-fixed'>
-        <div className='boxes-container'>
+      <footer className="footer footer-fixed">
+        <div className="boxes-container">
           <Button
-            className='user-button'
-            icon='fa-home'
+            className="user-button"
+            icon="fa-search"
             secondary
-            key='about'
-            iconClassName='fa fa-home'
-            href='/Emoticon3D-Material/'
+            key="about"
+            iconClassName="fa fa-search"
+            href="/Emoticon3D-Material/"
           >
-            <p>반갑다</p>
+            <p>모델 검색</p>
           </Button>
           <Button
-            className='user-button'
-            icon='fa-beer'
+            className="user-button"
+            icon="fa-beer"
             secondary
-            key='menu1'
-            iconClassName='fa fa-heart'
-            href='/Emoticon3D-Material/about'
+            key="menu1"
+            iconClassName="fa fa-heart"
+            href="/Emoticon3D-Material/about"
           >
-            <p>친구야</p>
+            <p>내3D모델</p>
           </Button>
 
           <Button
-            className='user-button'
-            icon='faHome' // ??
+            className="user-button"
+            icon="faHome" // ??
             secondary
-            key='variable-md-colors-warn'
-            iconClassName='fa fa-comment'
-            href='/Emoticon3D-Material/ThisIsNotAbout/about2'
+            key="variable-md-colors-warn"
+            iconClassName="fa fa-comment"
+            href="/Emoticon3D-Material/ThisIsNotAbout/about2"
           >
-            <p>일요일은</p>
+            <p>예약하기</p>
           </Button>
           <Button
-            className='user-button'
-            icon='faHome' // ??
+            className="user-button"
+            icon="faHome" // ??
             secondary
-            key='menu3'
-            iconClassName='fa fa-book'
-            href='/Emoticon3D-Material/the-butterfly-of-the-edge'
+            key="menu3"
+            iconClassName="fa fa-book"
+            href="/Emoticon3D-Material/the-butterfly-of-the-edge"
           >
-            <p>제발좀</p>
+            <p>출력상태</p>
           </Button>
           <Button
-            className='user-button'
-            icon='faHome' // ???
+            className="user-button"
+            icon="faHome" // ???
             secondary
-            key='menu4'
-            iconClassName='fa fa-tag'
-            href='/Emoticon3D-Material/-gatsby-material-starter'
+            key="menu4"
+            iconClassName="fa fa-tag"
+            href="/Emoticon3D-Material/-gatsby-material-starter"
           >
-            <p>쉬게해줘</p>
+            <p>더보기</p>
           </Button>
         </div>
       </footer>
--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,18 +1,18 @@
 /** src/layout/index.jsx */
-import React from 'react';
-import Helmet from 'react-helmet';
-import 'font-awesome/scss/font-awesome.scss';
-import { Link } from '@reach/router';
-import { FontIcon } from 'react-md';
+import React from "react";
+import Helmet from "react-helmet";
+import "font-awesome/scss/font-awesome.scss";
+import { Link } from "@reach/router";
+import { FontIcon } from "react-md";
 
-import Navigation from '../components/Navigation';
-import config from '../../data/SiteConfig';
-import './index.scss';
-import './global.scss';
+import Navigation from "../components/Navigation";
+import config from "../../data/SiteConfig";
+import "./index.scss";
+import "./global.scss";
 
-import BottomNav from '../components/BottomNav';
+import BottomNav from "../components/BottomNav";
 
-const TO_PREFIX = '';
+const TO_PREFIX = "";
 
 // navLinks에 항목을 추가/수정하면 하단바에도 적용됩니다.
 //아이콘은 여기서 https://fontawesome.com/icons?d=gallery
@@ -21,15 +21,13 @@ export default class MainLayout extends React.Component {
   render() {
     const { children } = this.props;
     return (
-      <Navigation config={config} LocalTitle={this.props.title}>
-        <div>
-          <Helmet>
-            <meta name='description' content={config.siteDescription} />
-          </Helmet>
-          {children}
-          <BottomNav />
-        </div>
-      </Navigation>
+      <div>
+        <Helmet>
+          <meta name="description" content={config.siteDescription} />
+        </Helmet>
+        {children}
+        <BottomNav />
+      </div>
     );
   }
 }
